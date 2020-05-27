9147f9850e22750871aa961ca10a24ead771b508
LuisvonPinto <luis.pinto.dgpc@gmail.com>
2020-05-27 18:13:39 -0500

    Eliminación de registros
    Peligro!

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,7 @@
+let registro_id, registro_nombre, registro_correo, registro_tr;
+
 $(function() {
-    $('.dropdown-trigger').dropdown({
+  $('.dropdown-trigger').dropdown({
     hover:false,
     inDuration:300,
     outDuration:350,
@@ -32,5 +34,43 @@ $(function() {
     invisible();
     $("#VistaAccesos").show('slow');
   });
+
+  $('.tooltipped').tooltip();
+  $('.modal').modal();
+
+  $('button.borrar').on('click', function() {
+    registro_id = null, registro_nombre = null, registro_correo = null, registro_tr = null;
+    registro_tr = $(this).parent().parent();
+    let tds = $(this).parent().siblings();
+    
+    registro_id = tds["0"].textContent;
+    registro_nombre = tds["1"].textContent;
+    registro_correo = tds["2"].textContent;
+    $('#borrar-registro-id').text('#'+registro_id);
+    $('#borrar-registro-nombre').text(registro_nombre);
+    $('#borrar-registro-correo').text(registro_correo);
+    $('#modal-borrar').modal('open');
+  })
+
+  $('#borrar-registro-aceptar').on('click', function() {
+    if (registro_id == null || registro_nombre == null || registro_correo == null){
+      alert("Ha ocurrido un error al tratar de eliminar este registro.");
+      return;
+    }
+    data = {id: registro_id, nombre: registro_nombre, correo: registro_correo, seguro_borrar: "sí"};
+    
+    $.post( "dash_fns.php", data, function(result) {
+      if ("status" in result && result["status"]){
+        alert( "Se ha eliminado el registro exitosamente." );
+        registro_tr.remove();
+      }
+      else {
+        alert( "No se ha podido eliminar este registro." );
+      }
+    }, 'json')
+      .fail(function() {
+        alert( "No se ha podido eliminar este registro." );
+      });
+  })
 
 });
